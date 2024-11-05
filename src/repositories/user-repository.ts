30d93e43ea46ecd7e3ad24f7a4f5cac47ea55c8b
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-11-05 17:24:39 +0900

    feat : add user repo functions

---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,23 @@
+import { Prisma, User } from "@prisma/client";
+
+import prisma from "./prisma";
+
+async function createData({
+  data,
+  select,
+}: {
+  data: Prisma.UserCreateInput;
+  select?: Prisma.UserSelect;
+}): Promise<User | null> {
+  return await prisma.user.create({ data, select });
+}
+
+async function findFirstData({
+  where,
+  select,
+}: {
+  where: Prisma.UserWhereInput;
+  select?: Prisma.UserSelect;
+}): Promise<User | null> {
+  return await prisma.user.findFirst({ where, select });
+}
