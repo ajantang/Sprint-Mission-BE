b3776c5330865e47644662ad7c1faba23c933f3d
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-11-15 11:56:15 +0900

    feat : set cors origin setting
    FE에서 토큰은 쿠키에 넣어서 사용하기로 결정

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,15 @@ import {
 
 const app: Express = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000/",
+      "https://sprint-fe-git-next-sprint11-snowrangs-projects.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/auth", authRouter);
